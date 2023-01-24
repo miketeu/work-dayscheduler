006ea0376337eac95871462d567f998fbbaefea5
Mike Teuten <mike.teuten@gmail.com>
2023-01-24 09:55:38 +0000

    added text input area.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 //The app should:
+$(document).ready(function () {
+  //* Display the current day at the top of the calender when a user opens the planner.
+  //Use UK time and date format.
+  setInterval(function () {
+    $("#currentTime").text(moment().format("LLLL"));
+  }, 1000);
 
-//* Display the current day at the top of the calender when a user opens the planner.
-//Use UK time and date format.
-setInterval(function () {
-  $("#currentTime").text(moment().format('LLLL'));
-}, 1000);
+  //End of time display.
 
-//End of time display.
+  // Change bg color in relation to time
+  // $("textarea").addClass("future")
+  // localStorage.setItem("9am", "text here")
 
-// $(element).css("backgroundColor",)
+  // $("button").on("click", function(event){
+  //console.log(event.target)
+
+  //localStorage.setItem("9am", "someTextHere");
+  //localStorage.getItem("9am");
+
+  
+});
